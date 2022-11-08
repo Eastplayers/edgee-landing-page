b5ca465da35efab3d9486e87f915dc3c34c2eb5a
Binh Nguyen <binhnguyen@Binhs-MacBook-Pro.local>
2022-11-09 01:24:49 +0700

    update-title-description-thumbnail

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,9 +15,37 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap"
             rel="stylesheet"
           />
+
+          <meta
+            property="og:image"
+            content="https://tribes-image.s3.ap-southeast-1.amazonaws.com/files/a1efee379d382f9610cbd7607f505d97.png"
+          />
+          <meta
+            property="og:title"
+            content="Edgee - Nền tảng chuyển đổi số toàn diện"
+          />
+          <meta
+            property="og:description"
+            content="Nền tảng Edgee hỗ trợ tích hợp các giải pháp số vào cốt lỗi của doanh nghiệp. Giúp tăng cơ hội và nâng cao năng lực cạnh tranh bền vững."
+          />
+
+          <meta
+            property="twitter:title"
+            content="Edgee - Nền tảng chuyển đổi số toàn diện"
+          />
+          <meta
+            property="twitter:description"
+            content="Nền tảng Edgee hỗ trợ tích hợp các giải pháp số vào cốt lỗi của doanh nghiệp. Giúp tăng cơ hội và nâng cao năng lực cạnh tranh bền vững."
+          />
+          <meta
+            property="twitter:image"
+            content="https://tribes-image.s3.ap-southeast-1.amazonaws.com/files/a1efee379d382f9610cbd7607f505d97.png"
+          />
+
+          <title>Edgee - Nền tảng chuyển đổi số toàn diện</title>
           <meta
             name="description"
-            content="Nhận tư vấn miễn phí từ đội ngũ chuyên gia chuyển đổi số Edgee"
+            content="Nền tảng Edgee hỗ trợ tích hợp các giải pháp số vào cốt lỗi của doanh nghiệp. Giúp tăng cơ hội và nâng cao năng lực cạnh tranh bền vững."
           />
           <link rel="icon" href="/favicon.ico" />
         </Head>
